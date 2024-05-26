711cddaa64d92b3ec8849286fcf82bb2b723bc55
SharpBlade <yepezedwin760@gmail.com>
2024-05-26 12:38:41 -0500

    Entidad estudiante para lista

--- src/students/entities/student.entity.ts
+++ src/students/entities/student.entity.ts
@@ -1,1 +1,22 @@
-export class Student {}
+import { Column, DeleteDateColumn, Entity } from 'typeorm';
+
+@Entity()
+export class Student {
+  @Column({ primary: true })
+  id: string;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: false })
+  career: string;
+
+  @Column({ default: 'Proceso' })
+  state: string;
+
+  @Column({ default: 0 })
+  percentage: number;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
