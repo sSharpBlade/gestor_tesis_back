66d2b8164e160b6c5c96fd9d26bd87ff8dcdf812
SharpBlade <yepezedwin760@gmail.com>
2024-05-26 12:39:21 -0500

    dto para estudiante

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,1 +1,19 @@
-export class CreateStudentDto {}
+import { IsString, MinLength, IsNumber } from 'class-validator';
+
+export class CreateStudentDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  career: string;
+
+  @IsString()
+  @MinLength(10)
+  id: string;
+
+  @IsString()
+  state: string;
+
+  @IsNumber()
+  percentage: number;
+}
