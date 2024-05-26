9df7cfce4f650c767ac9ebbdc9c37ce792ed2ddd
SharpBlade <yepezedwin760@gmail.com>
2024-05-26 12:38:05 -0500

    Conexión con la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [StudentsModule],
+  imports: [
+    StudentsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
