93fb64df2cd849e6d7048633cd11182e7827e38f
JaviF <fiallosedisonreggae@gmail.com>
2024-05-27 17:47:44 -0500

    Se agrego la busqueda de usuario por email

--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTeacherDto } from './dto/create-teacher.dto';
 import { UpdateTeacherDto } from './dto/update-teacher.dto';
+import { Teacher } from './entities/teacher.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+export interface UserFindOne{
+  id?: number;
+  email?: string;
+}
 
 @Injectable()
 export class TeachersService {
+
+  constructor(
+    @InjectRepository(Teacher)
+    private readonly teachersRepository: Repository<Teacher>
+){}
   create(createTeacherDto: CreateTeacherDto) {
     return 'This action adds a new teacher';
   }
@@ -22,5 +35,9 @@ export class TeachersService {
 
   remove(id: number) {
     return `This action removes a #${id} teacher`;
+  }
+
+  async findByEmail(email: string): Promise<Teacher | undefined> {
+    return this.teachersRepository.findOne({ where: { email } });
   }
 }
