b4d0db770c1baa46d82e9f8291af3578cf07f281
SharpBlade <yepezedwin760@gmail.com>
2024-06-14 17:22:13 -0500

    Bug en la columna de eliminación de reportes

--- src/reports/entities/report.entity.ts
+++ src/reports/entities/report.entity.ts
@@ -1,37 +1,38 @@
-import { Activities } from "src/activities/entities/activity.entity";
-import { Thesis } from "src/thesis/entities/thesis.entity";
+import { Activities } from 'src/activities/entities/activity.entity';
+import { Thesis } from 'src/thesis/entities/thesis.entity';
 import {
   Column,
+  DeleteDateColumn,
   Entity,
   Index,
   JoinColumn,
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
-} from "typeorm";
+} from 'typeorm';
 
 //@Index("reports_pk", ["idReport"], { unique: true })
-@Entity("reports", { schema: "public" })
+@Entity('reports', { schema: 'public' })
 export class Reports {
-  @PrimaryGeneratedColumn({ type: "integer", name: "id_report" })
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id_report' })
   idReport: number;
 
-  @Column("character varying", { name: "title", length: 50 })
+  @Column('character varying', { name: 'title', length: 50 })
   title: string;
 
-  @Column("timestamp without time zone", { name: "deletedAt", nullable: true })
+  @DeleteDateColumn()
   deletedAt: Date | null;
 
-  @Column("date", { name: "date" })
+  @Column('date', { name: 'date' })
   date: string;
 
-  @Column("integer", { name: "percentage" })
+  @Column('integer', { name: 'percentage' })
   percentage: number;
 
   @OneToMany(() => Activities, (activities) => activities.idReport)
   activities: Activities[];
 
   @ManyToOne(() => Thesis, (thesis) => thesis.reports)
-  @JoinColumn([{ name: "id_thesis", referencedColumnName: "idThesis" }])
+  @JoinColumn([{ name: 'id_thesis', referencedColumnName: 'idThesis' }])
   idThesis: Thesis;
 }
